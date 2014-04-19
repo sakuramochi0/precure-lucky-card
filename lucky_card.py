5869ce051b47e3e8e4ea610bcdc79ef520be84c6
sakuramochi0 <sakuramochi.mochi.0@gmail.com>
2014-04-19 20:36:31 +0900

    enable to download promo cards, add a switch for allstars card, add redownload()

--- lucky_card.py
+++ lucky_card.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # lucky_card.py - tweet a today's lucky DCD card
-import requests
-from PIL import Image
-from io import BytesIO
-from twython import Twython
-from bs4 import BeautifulSoup
 import re
 from os.path import basename, dirname, splitext, exists
 import yaml
@@ -13,6 +8,10 @@ from time import sleep
 import random
 from datetime import datetime
 from dateutil import parser
+import requests
+from PIL import Image
+from twython import Twython
+from bs4 import BeautifulSoup
 
 # settings
 db_file = 'cards.yaml'
@@ -21,12 +20,13 @@ log_file = 'tweet.log'
 img_dir = 'img/'
 cred_file = '.credentials'
 
+site_url = 'http://precure-live.com/allstars/cardlist/'
+site_url_with_category = 'http://precure-live.com/allstars/cardlist/happiness.php?search=true&category='
+site_img_url_base = 'http://precure-live.com/allstars/image/cardlist/'
+
 def download(series_id=''):
     '''Download data from dcd site, and update database.'''
     # download card data
-    site_url = 'http://precure-live.com/allstars/cardlist/'
-    site_url_with_category = 'http://precure-live.com/allstars/cardlist/happiness.php?search=true&category='
-    site_img_url_base = 'http://precure-live.com/allstars/image/cardlist/'
     if series_id:
         site_url = site_url_with_category + series_id
     if exists(db_file):
@@ -44,12 +44,17 @@ def download(series_id=''):
         series_name = basename(dirname(card.find_all('a')[0].img['src'])) # parent dirname i.e. 'happiness02'
         series_id = re.search(r'category=(\d+)', site_card_list_url).group(1) # 123456
         header = card.td.text.strip()
-        series_text, no, no_max = re.search(r'(.+)(\d{2})/(\d{2})', header).groups() # i.e. 'ハピネスチャージ2だん キュアハニー登場 22/48'
+        match = re.search(r'(.+)(\d{2})/(\d{2})', header) # i.e. 'ハピネスチャージ2だん キュアハニー登場 22/48'
+        if match:
+            series_text, no, no_max = match.groups()
+        else: # promo card
+            match = re.search(r'(.+)(\d{2})', header) # i.e. 'ハピネスチャージプロモ01'
+            series_text, no, no_max = match.group(1), match.group(2), 'promo'
         card_name = card.find(class_='cardname').text # 花がらドレス
         model = card.find(class_='item').text         # 愛乃めぐみ
-        img_front = basename(card.find_all('a')[0].img['src'])
-        img_back = basename(card.find_all('a')[1].img['src'])
-        img_both = splitext(img_front)[0] + '-both' + splitext(img_front)[1]
+        img_front = series_id + '-' + basename(card.find_all('a')[0].img['src'])
+        img_back = series_id + '-' + basename(card.find_all('a')[1].img['src'])
+        img_both = splitext(img_front)[0][:-2] + '-w' + splitext(img_front)[1]
         cardtype = basename(card.find_all(class_='icon')[0].img['src'])
         if cardtype == 'img_dress_tops-hc.jpg':
             cardtype = 'dress-tops-hc'
@@ -57,11 +62,16 @@ def download(series_id=''):
             cardtype = 'dress-bottoms-hc'
         elif cardtype == 'ico_allstars.jpg':
             cardtype = 'allstars'
-        rarity = basename(card.find_all(class_='icon')[1].img['src'])
-        if rarity == 'img_rare_n-hc.gif':
-            rarity = 'n-hc'
-        elif rarity == 'img_rare_prc-hc.gif':
-            rarity = 'prc-hc'
+        rarity_icon = card.find_all(class_='icon')[1].img
+        if rarity_icon:
+            rarity = basename(rarity_icon['src'])
+            if rarity == 'img_rare_n-hc.gif':
+                rarity = 'n-hc'
+            elif rarity == 'img_rare_prc-hc.gif':
+                rarity = 'prc-hc'
+        else: # promo card
+            rarity = False
+
         card_text = card.find(class_='card_txt').text # 素敵なカードだよ
         card_id = series_id + '-' + no # i.e. 123456-22
         if card_id in cards.keys():    # avoid double download
@@ -69,7 +79,7 @@ def download(series_id=''):
             continue
         else:
             cards[card_id] = {'series_name': series_name,
-                                    'series_id': series_id,
+                              'series_id': series_id,
                               'series_text': series_text,
                               'no': no,
                               'no_max': no_max,
@@ -78,34 +88,27 @@ def download(series_id=''):
                               'img_front': img_front,
                               'img_back': img_back,
                               'img_both': img_both,
+                              'img_url': False,
                               'cardtype': cardtype,
                               'rarity': rarity,
                               'card_text': card_text}
-        print('='*8)
         print('Get a new card.')
-        print('='*8)
         print(cards[card_id])
-        # download and concatenate card image
-        site_img_urls = [site_img_url_base + series_name + '/' + img_front,
-                         site_img_url_base + series_name + '/' + img_back]
-        imgs = []
-        for site_img_url in site_img_urls:
-            r = requests.get(site_img_url)
-            imgs.append(Image.open(BytesIO(r.content)))
-        w = sum(i.size[0] for i in imgs) + 20
-        mh = max(i.size[1] for i in imgs)
-        res = Image.new("RGBA", (w,mh))
-        x = 0
-        for i in imgs:
-            res.paste(i, (x, 0))
-            x += (i.size[0] + 20)
-        res.save(img_dir + img_both)
+
+        filenames = [img_front, img_back]
+        for filename in filenames:
+            img_url = site_img_url_base + series_name + '/' + filename[7:]
+            r = requests.get(img_url)
+            with open(img_dir + filename, 'wb') as f:
+                f.write(r.content)
+
+        img_concatenate(img_front, img_back, img_both)
 
         # add to new_cardlist
         new_cardlist.append('{}「{}」'.format(model, card_name))
         
+        print('Download and concatenamhte images:', img_both)
         print('='*8)
-        print('Download and concatenate two card image:', img_both)
         sleep(0.5)
         
     # write db
@@ -123,8 +126,39 @@ def download(series_id=''):
             sleep(1)
         shuffle()
         
+def img_concatenate(front, back, both):
+    '''concatenate front and back images and save to both'''
+    img_front = Image.open(img_dir + front)
+    img_back = Image.open(img_dir + back)
+        
+    w = img_front.size[0] * 2 + 10
+    h = img_front.size[1]
+    img_both = Image.new("RGBA", (w, h))
+    x = 0
+    img_both.paste(img_front, (x, 0))
+    x += img_front.size[0] + 10
+    img_both.paste(img_back, (x, 0))
+    img_both.save(img_dir + 'both/' + both)
+
+def redownload():
+    '''re-download all the images'''
+    with open(db_file) as db:
+        cards = yaml.load(db)
+    for (id, card) in cards.items():
+        img_front = card['img_front']
+        img_back = card['img_back']
+        img_both = card['img_both']
+        for img in (img_front, img_back):
+            img_url = site_img_url_base + card['series_name'] + '/' + img[7:]
+            r = requests.get(img_url)
+            with open(img_dir + img, 'wb') as f:
+                f.write(r.content)
+        img_concatenate(img_front, img_back, img_both)
+        print('Donwload and concatenate both images:', img_both)
+        sleep(0.5)
+
 def shuffle():
-    '''Create a list of shuffled cards to make a que file.'''
+    '''Generate a shuffled que list.'''
     with open(db_file) as db:
         cards = yaml.load(db)
     card_ids = list(cards.keys())
@@ -154,10 +188,6 @@ def tweet(status=''):
             cards = yaml.load(db)
         card = cards[card_id]
         img_both = card['img_both']
-        if 'img_url' in card.keys():
-            img_url = card['img_url']
-        else:
-            img_url = ''
         model = card['model']
         card_name = card['card_name']
         card_text = card['card_text']
@@ -165,18 +195,26 @@ def tweet(status=''):
         # generate status
         morning = datetime.now().hour < 16
         if morning:
-            status = '今日のラッキーカードは、{model}の「{cn}」のカードです！「{ct}」今日がハッピーな一日になりますように！'\
-                .format(model=model, cn=card_name, ct=card_text)
+            if model == card_name:
+                status = '今日のラッキーカードは、{model}のカードです！\
+今日がハッピーな一日になりますように！'.format(model=model, ct=card_text)
+            else:
+                status = '今日のラッキーカードは、{model}の「{cn}」のカードです！\
+「{ct}」今日がハッピーな一日になりますように！'.format(model=model, cn=card_name, ct=card_text)
         else:
-            status = '今日も一日お疲れさま！今日のラッキーカードは、{model}の「{cn}」のカードでした。ハッピーな一日にできたかな？'\
-                .format(model=model, cn=card_name, ct=card_text)
+            if model == card_name:
+                status = '今日も一日お疲れさま！今日のラッキーカードは、{model}のカードでした。\
+ハッピーな一日にできたかな？'.format(model=model)
+            else:
+                status = '今日も一日お疲れさま！今日のラッキーカードは、{model}の「{cn}」のカードでした。\
+ハッピーな一日にできたかな？'.format(model=model, cn=card_name)
 
         # tweet
-        if img_url:
-            status += ' ' + img_url
+        if card['img_url']:
+            status += ' ' + card['img_url']
             res = t.update_status(status=status)
         else:
-            img = open(img_dir + img_both, 'rb')
+            img = open(img_dir + 'both/' + img_both, 'rb')
             res = t.update_status_with_media(status=status, media=img)
             img_url = res['entities']['media'][0]['url']
             # add img_url to db
@@ -210,8 +248,8 @@ def clear():
     '''Clear img_url to re-upload image files.'''
     with open(db_file) as db:
         cards = yaml.load(db)
-    for card_id in cards.keys():
-        del cards[card_id]['img_url']
+    for (id, card) in cards.items():
+        card['img_url'] = False
     with open(db_file, 'w') as db:
         yaml.dump(cards, db)
                 
@@ -219,12 +257,20 @@ if __name__ == '__main__':
     usage = '''\
 usage: {0} [test] <command> [<args>]
 command:
-  download [<series_id>]  Download cards data and images from the site.
-  tweet [<status>]        Tweet a que[status].
-  shuffle                 Generate a shuffled que list.
-  clear                   Clear img_url from the db_file.
+  download [<series_id>]  {1}
+  tweet [<status>]        {2}
+  redownload              {3}
+  shuffle                 {4}
+  clear                   {5}
 test:
-  if 'test' is given as the first argument, post tweet to the test account.'''.format(basename(sys.argv[0]))
+  if 'test' is given as the first argument, post tweet to the test account.'''.format(
+          basename(sys.argv[0]),
+          download.__doc__,
+          tweet.__doc__,
+          redownload.__doc__,
+          shuffle.__doc__,
+          clear.__doc__,)
+
     if len(sys.argv) == 1:
         print(usage)
     else:
@@ -245,6 +291,8 @@ test:
                 tweet(sys.argv[2])
             else:
                 tweet()
+        elif sys.argv[1] == 'redownload':
+            redownload()
         elif sys.argv[1] == 'shuffle':
             shuffle()
         elif sys.argv[1] == 'clear':
